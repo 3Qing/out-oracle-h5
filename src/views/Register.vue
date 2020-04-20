465d587b09da0102881ef1cf7a2daf404a9a0faf
donghuanjie1995@163.com <wangchenqing1994@outlook.com>
2020-04-20 21:35:00 +0800

    注册表单验证

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,14 +3,14 @@
         <div class="logo">
             <img src="../assets/logo.jpg" alt="神谕量化">
         </div>
-        <a-form-model class="form-wrapper" :ref="form" :model="from" :rules="rules">
-            <a-form-model-item class="form-item">
+        <a-form-model class="form-wrapper" ref="form" :model="form" :rules="rules">
+            <a-form-model-item class="form-item" prop="phone">
                 <a-input placeholder="请输入手机号码" v-model="form.phone"></a-input>
             </a-form-model-item>
-            <a-form-model-item class="form-item">
+            <a-form-model-item class="form-item" prop="password">
                 <a-input-password placeholder="请输入密码" v-model="form.password"></a-input-password>
             </a-form-model-item>
-            <a-form-model-item class="form-item">
+            <a-form-model-item class="form-item" prop="password2">
                 <a-input-password placeholder="确认密码" v-model="form.password2"></a-input-password>
             </a-form-model-item>
             <a-form-model-item class="form-item">
@@ -50,6 +50,41 @@
 <script>
 export default {
     data() {
+        let validatePhone = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请输入手机号码'))
+            } else {
+                let _regPhone = /^[1][3,4,5,7,8][0-9]{9}$/
+                if (this.form.phone !== '' && _regPhone.test(value)) {
+                    this.$refs.form.validateField('phone')
+                    callback()
+                } else {
+                    callback(new Error('请输入正确的手机号码'))
+                }
+            }
+        }
+        let validatePass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请输入密码'))
+            } else {
+                let _regPass = /^[0-9a-zA-Z]*$/g
+                if (this.form.password !== '' && _regPass.test(value)) {
+                    this.$refs.form.validateField('checkPass')
+                    callback()
+                } else {
+                    callback(new Error('密码应为数字或字母'))
+                }
+            }
+        }
+        let validatePass2 = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请确认密码'))
+            } else if (value !== this.form.password) {
+                callback(new Error("两次密码不一致"))
+            } else {
+                callback()
+            }
+        }
         return {
             form: {
                 phone: '',
@@ -57,7 +92,24 @@ export default {
                 password2: '',
                 code: ''
             },
-            rules: {},
+            rules: {
+                phone: [
+                    // { required: true, message: '请输入手机号码', trigger: 'blur' },
+                    { validator: validatePhone, trigger: 'change' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 6, message: '密码长度至少6位', trigger: 'blur' },
+                    { validator: validatePass, trigger: 'change' }
+                ],
+                password2: [
+                    // { required: true, message: '请确认密码', trigger: 'blur' },
+                    { validator: validatePass2, trigger: 'change' }
+                ],
+                // code: [
+                //     { required: true, message: '请输入验证码', trigger: 'blur' }
+                // ]
+            },
             tip: '注册中...',
             vcode: '',
             loading: false,
@@ -76,6 +128,16 @@ export default {
     },
     computed: {
         disabled() {
+            // if (this.vcode) {
+            //     this.$refs.form.validate(valid => {
+            //         if (valid) {
+            //             return false
+            //         } else {
+            //             return true
+            //         }
+            //     })
+            // }
+            // return true
             return !this.form.phone || !this.form.password || !this.form.password2 || !this.form.code || !this.vcode
         },
         vcodeDisabled() {
@@ -87,15 +149,21 @@ export default {
             this.$router.push({ name: 'Login' })
         },
         beforeSubmit() {
-            if (this.form.password !== this.form.password2) {
-                this.$message.warning('两次密码不一致')
-                return
-            }
+            // if (this.form.password !== this.form.password2) {
+            //     this.$message.warning('两次密码不一致')
+            //     return
+            // }
             this.$refs.form.validate(valid => {
                 if (valid) {
                     if (this.vcode) {
                         this.verifyCode()
+                    } else {
+                        this.$message.warning('请输入验证码')
+                        return
                     }
+                } else {
+                    this.$message.warning('请正确填写表单内容')
+                    return
                 }
             })
             // if (!this.verify) {
